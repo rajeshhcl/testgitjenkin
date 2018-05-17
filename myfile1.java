7ce2b891b5e46bf7299c2433d189d521c13f4a4a
Rajesh Ramadoss <raramad@wipro.com>
2018-05-17 11:30:59 +0100

    changes part of gitbranch1

--- myfile1.java
+++ myfile1.java
@@ -3,5 +3,6 @@ public class myfile1
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, welcome to DevOps 4th day while testing github & jenkins");
+		System.out.println("added branch to github project from gitbranch1");
 	}
 }
