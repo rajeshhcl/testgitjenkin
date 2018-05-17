94da838d4e621df56577516dd387c65ddd35c14e
Rajesh Ramadoss <raramad@wipro.com>
2018-05-17 11:24:15 +0100

    testing jenkin & github after cloning from github and changing code

--- myfile1.java
+++ myfile1.java
@@ -2,6 +2,6 @@ public class myfile1
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, welcome to DevOps 2nd session");
+		System.out.println("Hello, welcome to DevOps 4th day while testing github & jenkins");
 	}
 }
